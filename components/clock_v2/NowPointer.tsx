6c11c0ef0d7eeb7e912bfdebde98c1e888e304e7
Yu Wei Teh <yuwei@netizenexperience.com>
2024-10-07 14:20:18 +0800

    Add time pointer click and drag event handler

--- components/clock_v2/NowPointer.tsx
+++ components/clock_v2/NowPointer.tsx
@@ -1,6 +1,7 @@
 import { CENTER_CIRCLE_SIZE, LARGEST_CIRCLE_SIZE } from "@/constants/Constants";
 import { useScreenSize } from "@/contexts/ScreenSizeContext";
 import { useEffect, useState } from "react";
+import { GestureResponderEvent, PanResponder, View } from "react-native";
 import { Circle, Line, Svg } from "react-native-svg";
 
 export default function NowPointer({
@@ -19,6 +20,31 @@ export default function NowPointer({
     ...getPointerEndPosition(),
   });
   
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant(e, gestureState) {
+      // when user click anywhere within the clock
+      console.log("👆👆 onPanResponderGrant");
+      handlePointerInteraction(e);
+    },
+    onPanResponderMove: (e, gestureState) => {
+      // when user drags the pointer
+      console.log("💧💧 onPanResponderMove");
+      handlePointerInteraction(e);
+    }, 
+  })
+
+  function handlePointerInteraction(e: GestureResponderEvent) {
+    const { locationX, locationY } = e.nativeEvent;
+    const dx = locationX - position.startX;
+    const dy = locationY - position.startY;
+    const pointerAngle = Math.atan2(dy, dx) * 180 / Math.PI;
+    const pointerAngleRadian = (pointerAngle) * (Math.PI / 180);
+    const endX = radius + radius * Math.cos(pointerAngleRadian);
+    const endY = radius + radius * Math.sin(pointerAngleRadian);
+    setPosition(prevPosition => ({ ...prevPosition, endX, endY }));   
+  }
+
   function getPointerEndPosition() {
     const timeNow = new Date();
     const hours = timeNow.getHours();
@@ -48,30 +74,25 @@ export default function NowPointer({
   }, [])
   
   return (
-    <Svg
-      width={size}
-      height={size}
-      style={[
-        { top: containerHeight / 2 - size / 2 },
-        { left: screenSize.width / 2 - size / 2 },
-      ]}
-      className="z-10"
-    >
-      <Circle 
-        id="center-dot"
-        cx={radius}
-        cy={radius}
-        r={10}
-        fill="white"
+    <View className="z-10" {...panResponder.panHandlers}>
+      <Svg
+        width={size}
+        height={size}
+        style={[
+          { top: containerHeight / 2 - size / 2 },
+          { left: screenSize.width / 2 - size / 2 },
+        ]}
+      >
+        <Circle id="center-dot" cx={radius} cy={radius} r={10} fill="white" />
+        <Line
+          x1={position.startX}
+          y1={position.startY}
+          x2={position.endX}
+          y2={position.endY}
+          stroke="white"
+          strokeWidth={3}
         />
-      <Line
-        x1={position.startX}
-        y1={position.startY}
-        x2={position.endX}
-        y2={position.endY}
-        stroke="white"
-        strokeWidth={3}
-      />
-    </Svg>
+      </Svg>
+    </View>
   );
 }
