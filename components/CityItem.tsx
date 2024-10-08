f7745057ca8f4c0dba472e0d4c208854e945b523
Yu Wei Teh <yuwei@netizenexperience.com>
2024-10-08 11:48:07 +0800

    Link circular ring color with city item

--- components/CityItem.tsx
+++ components/CityItem.tsx
@@ -1,4 +1,4 @@
-import { City } from "@/constants/Constants";
+import { City, COLOR_SEQUENCE } from "@/constants/Constants";
 import { useScreenSize } from "@/contexts/ScreenSizeContext";
 import { View, Text } from "react-native";
 
@@ -6,13 +6,19 @@ export interface CityItemType extends City {
   currentTime: number;
 }
 
-export default function CityItem({ city }: { city: CityItemType }) {
+export default function CityItem({
+  city,
+  Nth,
+}: {
+  city: CityItemType;
+  Nth: number;
+}) {
   const { width } = useScreenSize();
 
   return (
     <View
-      className=" bg-[#31394a] mx-auto mb-2 border border-red-500 rounded-xl py-2 px-3"
-      style={{ width: width * 0.9 }}
+      className=" bg-[#31394a] mx-auto mb-2 border-2 rounded-xl py-2 px-3"
+      style={{ width: width * 0.9, borderColor: COLOR_SEQUENCE[Nth-1] }}
     >
       <View className="flex flex-row justify-between">
         <Text className="text-white font-light">{city.name}</Text>
--- components/CityList.tsx
+++ components/CityList.tsx
@@ -7,7 +7,7 @@ export default function CityList({ cities }: { cities: CityItemType[] }) {
   return (
     <View className="flex mt-2">
       { cities.map((city, i) => (
-        <CityItem key={i} city={city}  />  
+        <CityItem key={i} city={city} Nth={i+1} />  
       ))}
     </View>
   );
