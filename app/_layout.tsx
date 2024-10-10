0d157a223f77b2314b10c1c1828b677d7e3edf25
Yu Wei Teh <yuwei@netizenexperience.com>
2024-10-10 09:59:10 +0800

    Add light status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { ScreenSizeProvider } from "@/contexts/ScreenSizeContext";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { NativeWindStyleSheet } from "nativewind";
 
 NativeWindStyleSheet.setOutput({
@@ -12,6 +13,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
       </Stack>
+      <StatusBar style="light" />
     </ScreenSizeProvider>
   );
 }
